eadbcdd3baaac0397d3804071df27b4b0ab1c435
reniz-vsgt <reniz@innoscent.in>
2024-04-24 16:19:10 +0530

    co2 graph done

--- src/Pages/BLE/BLE.tsx
+++ src/Pages/BLE/BLE.tsx
@@ -1,13 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import { BluetoothDevice, BluetoothRemoteGATTCharacteristic, BluetoothRemoteGATTServer, IBleProps, RequestDeviceOptions } from './BLE.types';
-import { Space, Typography, Button, Modal, Form, Input } from 'antd';
+import { Space, Typography, Button, Modal, Form, Input, FormProps } from 'antd';
 import { Layout } from 'antd';
 import { cardio } from 'ldrs'
 import './BLE.css'
+import { LineChart } from '@mui/x-charts/LineChart';
+
 
 cardio.register()
 
 
+export interface IFormData {
+    name: string
+    age: number
+    height: number
+    weight: number
+}
 
 const { Title } = Typography;
 const { Content } = Layout;
@@ -36,6 +44,8 @@ const BLE: React.FC<IBleProps> = ({
     const [seconds, setSeconds] = useState<number>(0)
     const [min, setMin] = React.useState<number>(0);
     const [sec, setSec] = React.useState<number>(0);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
 
     const [service, setService] = useState<BluetoothRemoteGATTServer | undefined>();
 
@@ -43,11 +53,52 @@ const BLE: React.FC<IBleProps> = ({
 
     const [readChar, setReadChar] = useState<BluetoothRemoteGATTCharacteristic>();
     const [timer, setTimer] = useState<NodeJS.Timer>()
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const [sensorCommand, setSensorCommand] = useState<string>("")
+    const [startTimestamp, setStartTimestamp] = useState<string>("")
+    const [formData, setFormData] = useState<IFormData | null>(null)
+
+    const [graphData, setGraphData] = useState<any>(null)
 
 
 
+
+    const baseUrl = "https://rnd-api.breathai.io"
+    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2c2d0IjoieydfaWQnOiAnNjYyOGM0MDBkMDE1MzE4MzQyYmFiYTA1JywgJ3VzZXJuYW1lJzogJ3JlYWN0LWJsZScsICdlbWFpbCc6ICdyZW5pekBpbm5vc2NlbnQuaW4nLCAncGFzc3dvcmQnOiAnJDJiJDEyJFBKN21sN2VvcEx6TUpGcGZILndJOC5jb2hWSWFWNndsTEo2QXQ5MXVxd29qOTFnaWljQVcuJywgJ2Rpc3BsYXlOYW1lJzogJycsICdhZ2UnOiAnJywgJ2RhdGVPZkJpcnRoJzogJycsICdnZW5kZXInOiAnJywgJ2NvbnRhY3RJbmZvcm1hdGlvbic6ICcnLCAnbWVkaWNhbENvbmRpdGlvbnMnOiAnJywgJ21lZGljYXRpb25zJzogJycsICdmaXRuZXNzR29hbHMnOiAnJywgJ2ZpdG5lc3NQbGFuJzogJycsICdjaHJvbmljSGVhbHRoQ29uZGl0aW9ucyc6ICcnLCAnbWVkaWNhdGlvbkhpc3RvcnknOiAnJywgJ2RpZXRhcnlIYWJpdHMnOiAnJywgJ3NsZWVwUXVhbGl0eSc6ICcnLCAnbm90ZXNDb21tZW50cyc6ICcnLCAnY3JlYXRlZEF0JzogZGF0ZXRpbWUuZGF0ZXRpbWUoMjAyNCwgNCwgMTgsIDEyLCA0LCAzMCwgODQxMDAwKSwgJ2NyZWF0ZWRCeSc6ICdzeXN0ZW0nLCAndXBkYXRlZEF0JzogZGF0ZXRpbWUuZGF0ZXRpbWUoMjAyNCwgNCwgMTgsIDEyLCA0LCAzMCwgODQxMDAwKSwgJ3VwZGF0ZWRCeSc6ICdzeXN0ZW0nLCAnaXNBY3RpdmUnOiBUcnVlLCAnaXNEZWxldGVkJzogRmFsc2V9In0.PpKpLrRaaej98Ry6UuKme7lJfb7LaBf8dhqZpsMKT0g"
+
+
+    const uploadFile = async (fileData: Uint8Array) => {
+        const myHeaders = new Headers();
+        myHeaders.append("Accept", "application/json");
+        myHeaders.append("Authorization", "Bearer " + token);
+
+        const formdata = new FormData();
+        formdata.append("binFile", new Blob([fileData]), "upload.bin");
+
+        const requestOptions = {
+            method: "POST",
+            headers: myHeaders,
+            body: formdata,
+        };
+
+        const deviceId = device?.name;
+
+        try {
+            const response = await fetch(
+                `${baseUrl}/api/v2/vsgt-recording-service/uploadCo2BinFile?deviceId=${deviceId}&startTime=${startTimestamp}&name=${formData?.name}&age=${formData?.age}&height=${formData?.height}&weight=${formData?.weight}`,
+                requestOptions
+            );
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+
+            const data = await response.json();
+            setGraphData(data)
+            console.log(data);
+        } catch (error) {
+            console.error("Error:", error);
+        }
+
+    }
 
     const makeTimeForm = (time: number): void => {
         if (time < 60) {
@@ -150,12 +201,20 @@ const BLE: React.FC<IBleProps> = ({
             alert('Please connect a device first');
             return;
         }
+        if (!formData) {
+            console.error('Please enter subject details first');
+            alert('Please enter subject details first');
+            return;
+        }
         try {
             if (service) {
-
                 try {
                     await readChar?.startNotifications();
                     startTimer()
+                    const today = (new Date().toISOString())
+                    const timestamp = today.substring(0, today.length - 1);
+                    setStartTimestamp(timestamp)
+
                     readChar?.addEventListener('characteristicvaluechanged', (event) => {
                         const val = (event.target as BluetoothRemoteGATTCharacteristic).value?.buffer;
                         if (val) {
@@ -182,23 +241,12 @@ const BLE: React.FC<IBleProps> = ({
     const stopTimer = () => {
         setLoader(false)
         clearInterval(timer)
-        download(finalData, device?.name + ".bin")
+        // download(finalData, device?.name + ".bin")
+        uploadFile(finalData)
         setFinalData(new Uint8Array(0));
 
     }
 
-    const download = (data: any, filename: string) => {
-        const blob = new Blob([data], { type: 'application/octet-stream' });
-        const url = URL.createObjectURL(blob);
-        const a = document.createElement('a');
-        a.href = url;
-        a.download = filename;
-        document.body.appendChild(a);
-        a.click();
-        document.body.removeChild(a);
-        URL.revokeObjectURL(url);
-
-    };
     const startTimer = async () => {
         setLoader(true)
         const intervalId = setInterval(async () => {
@@ -211,6 +259,13 @@ const BLE: React.FC<IBleProps> = ({
         setIsModalOpen(false);
     };
 
+    const onFinish: FormProps<IFormData>['onFinish'] = (values) => {
+        setFormData(values)
+        setIsModalOpen(false)
+    };
+
+
+
     return (
         <>
 
@@ -221,12 +276,12 @@ const BLE: React.FC<IBleProps> = ({
                         <Button style={{ backgroundColor: "#83BF8D" }} type="primary" size={'large'} onClick={connectToDevice}>Connect to Device</Button>
                         {device != null ? (
                             <>
-                                {/* <Button style={{ backgroundColor: "#83BF8D" }} type="primary" size={'large'} onClick={() => setIsModalOpen(true)}>Enter Details</Button> */}
+                                <Button style={{ backgroundColor: "#83BF8D" }} type="primary" size={'large'} onClick={() => setIsModalOpen(true)}>Enter Details</Button>
                                 <Button style={{ backgroundColor: "#83BF8D" }} type="primary" size={'large'} onClick={readCharacteristic}>Subscribe</Button>
-                                {/* <Button style={{ backgroundColor: "#83BF8D" }} type="primary" size={'large'} onClick={() => writeCharacteristic(sensorCommand)}>Write</Button> */}
                                 <Button style={{ backgroundColor: "#83BF8D" }} type="primary" size={'large'} onClick={() => writeCharacteristic(writeValue)}>Write</Button>
                                 <Button style={{ backgroundColor: "#83BF8D" }} type="primary" size={'large'} onClick={stopTimer}>Stop Reading</Button>
                                 <br />
+
                             </>
                         ) : null}
                     </Space>
@@ -261,19 +316,72 @@ const BLE: React.FC<IBleProps> = ({
                         </div>
                     )}
 
+                    {graphData && (
+                        <LineChart
+                        xAxis={[{ data: graphData.payload.ticks }]}
+                        series={[
+                          {
+                            data: graphData.payload.co2_percentage,
+                          },
+                        ]}
+                        height={600}
+                        width={900}
+                        margin={{ left: 30, right: 30, top: 30, bottom: 30 }}
+                        grid={{ vertical: true, horizontal: true }}
+                      />
+                    )}
+
                 </Content>
             </Layout>
+
             <Modal title="Enter Details" open={isModalOpen} footer={null} onCancel={handleCancel}>
-                <Form>
-                    <Form.Item label="Enter Sensors command">
-                        <Input placeholder="Enter Sensors command" onChange={(e) => setSensorCommand(e.target.value)} />
+                <Form
+                    name="basic"
+                    labelCol={{ span: 8 }}
+                    wrapperCol={{ span: 16 }}
+                    style={{ maxWidth: 600 }}
+                    initialValues={{ remember: true }}
+                    onFinish={onFinish}
+                    autoComplete="off"
+                >
+                    <Form.Item
+                        label="Name"
+                        name="name"
+                        rules={[{ required: true, message: 'Please input your name!' }]}
+                    >
+                        <Input />
                     </Form.Item>
-                    <Form.Item>
-                        <Button style={{ backgroundColor: "#83BF8D" }} type="primary" onClick={handleCancel}>Submit</Button>
+                    <Form.Item
+                        label="Age"
+                        name="age"
+                        rules={[{ required: true, message: 'Please input your age!' }]}
+                    >
+                        <Input type='number' />
+                    </Form.Item>
+                    <Form.Item
+                        label="Height"
+                        name="height"
+                        rules={[{ required: true, message: 'Please input your height!' }]}
+                    >
+                        <Input type='number' />
+                    </Form.Item>
+                    <Form.Item
+                        label="Weight"
+                        name="weight"
+                        rules={[{ required: true, message: 'Please input your weight!' }]}
+                    >
+                        <Input type='number' />
+                    </Form.Item>
+
+
+
+                    <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                        <Button type="primary" htmlType="submit">
+                            Submit
+                        </Button>
                     </Form.Item>
                 </Form>
             </Modal>
-
         </>
 
     );
